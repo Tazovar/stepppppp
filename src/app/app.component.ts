44d699d17aa2462e9829f4eaafd8b0708c56e67a
Tazo gelashvili <120113889+Tazovar@users.noreply.github.com>
2025-04-06 19:21:51 +0400

    asfasfas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -9,5 +9,50 @@ import { Component } from '@angular/core';
   standalone:false
 })
 export class AppComponent {
-  
+  testType:string = 'text'
+  testPlaceholder:string = 'text...'
+  testValue:string | number = 'giorgi gelashvili'
+  testValue2:string | number = this.testValue
+  showHideText:string = 'hide';
+  counter:number = 0;
+  // name:string = 'tazo gelashvili';
+
+  // number1:number = 50;
+  // number2:number = 23;
+
+
+  // getUserName(){
+  //   return this.name
+  // }
+
+
+  // getNumber1PlusNumber2(){
+  //   return this.number1 + this.number2;
+  // }
+
+
+  onTestBtnClick(){
+    this.showHideText = this.showHideText == 'hide' ? 'show' : 'hide'
+   this.testType = this.testType == 'text' ? 'password' : 'text'
+  }
+
+  onTestBtncMouseMove(){
+    if(this.counter == 0){
+      this.testValue2 = this.testValue
+    }
+    console.log(this.testValue2);
+    
+    this.counter++
+    this.testValue = Math.floor(Math.random() * 100)
+  }
+
+
+  onTestBtnMouseLeave(){
+    this.counter = 0;
+    this.testValue = this.testValue2
+  }
+
+  // onTestInpInput(e:any){
+  //   this.testValue = e.target.value
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/main/main.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderSerachComponent } from './components/header/components/header-serach/header-serach.component';
-
+import { FormsModule } from '@angular/forms'
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MainComponent,
-    FooterComponent,
-    HeaderSerachComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
