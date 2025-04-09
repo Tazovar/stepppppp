58383ebfbe715d530c5403622b36b6cde434fd68
Tazo gelashvili <120113889+Tazovar@users.noreply.github.com>
2025-04-09 19:56:31 +0400

    asfasf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { MainComponent } from './components/main/main.component';
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
